d0515af4816fbb205447ee29937e13da6895f7f7
Monty Zukowski <monty@washdish.com>
2026-04-15 15:46:46 -0700

    good

--- hollow_cylinder_4in.py
+++ hollow_cylinder_4in.py
@@ -11,33 +11,6 @@ cylinder_radius = 4.0/2.0*25.4 # Radius of extruded cylinder outer wall
 chamfer_size = 10.0
 chamfer_depth = 20.0
 
-result5 = ( cq.Workplane("XY")
-        .circle(cylinder_radius - wall_thickness)
-        .extrude(thickness)
-        .tag("base")
-        .edges(">Z").chamfer(chamfer_depth, chamfer_size)
-        .transformed(rotate=(0, 180, 0))
-        .text(
-                """4.0" dia. tube\n3/8" thick""",       # The text string
-                fontsize=12,    # Size of the text
-                distance=0.33,    # Extrusion distance (depth)
-                combine=True
-            )
-        .faces("<Z", tag="base").workplane()
-        .transformed(rotate=(0, 180, 0))
-        .circle(die_radius)
-        .circle(cylinder_radius)
-        .extrude(thickness)
-        .faces(">Z", tag="base").workplane()
-        .transformed(offset=(0,0,-0.125*thickness))
-        .box(cylinder_radius*2,fin_thickness,thickness/4.0)
-        .faces(">Z", tag="base").workplane()
-        .transformed(offset=(0,0,-0.125*thickness))
-        .box(fin_thickness,cylinder_radius*2,thickness/4.0)
-)
-
-result4 = cq.Workplane("XY").box(cylinder_radius*2.0,fin_thickness,thickness/4.0).edges("|X and <Z").chamfer(3, fin_thickness/2.5)
-
 part1 = (
     cq.Workplane("XY")
     .circle(cylinder_radius - wall_thickness)
@@ -51,12 +24,12 @@ text1 = (
     .text(
         """4.0" dia. tube\n3/8" thick""",  # The text string
         fontsize=12,  # Size of the text
-        distance=0.33,  # Extrusion distance (depth)
+        distance=-0.33,  # Extrusion distance (depth)
     )
     .val()
     .moved(ry=180)
 )
-part1 = part1.union(text1)
+part1 = part1.cut(text1)
 
 
 part2 = (
@@ -72,7 +45,7 @@ part2 = (
         .chamfer(3, fin_thickness / 2.5)
     )
     .val()
-    .moved(z=thickness - thickness / 4.0)
+    .moved(z=thickness - thickness / 2.0)
 )
 
 part3 = (
@@ -88,7 +61,7 @@ part3 = (
         .chamfer(3, fin_thickness / 2.5)
     )
     .val()
-    .moved(z=thickness - thickness / 4.0)
+    .moved(z=thickness - thickness / 2.0)
 )
 
 
